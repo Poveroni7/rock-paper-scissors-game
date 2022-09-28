e79c63aac5f038528c0490cbe964a43663c4638c
Poveroni7 <114050508+Poveroni7@users.noreply.github.com>
2022-09-28 08:19:04 +0300

    Now game repeats

--- game.py
+++ game.py
@@ -1,8 +1,15 @@
-human_turn = 'rock'
-computer_turn = 'scissors'
+
+import random
+turns = ['rock', 'paper', 'scissors']
+while(True):
+ human_turn = input ('Enter your turn, or type exit: ')
+ computer_turn = random.choice(turns)
+ print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+if human_turn == 'exit':
+    print('Thank you for playing! Bye Bye')
+    'break'
 if human_turn == computer_turn:
     print('Draw!')
-
 if human_turn == 'rock' and computer_turn == 'scissors':
     print('Human wins!')
 elif human_turn == 'paper' and computer_turn == 'rock':
